47749e0a2d768a712c085099042a896bc3e86e58
medlying <medlying@gmail.com>
2019-05-21 21:30:38 +0800

    build project

---
+++ pro-deer-api/src/main/java/com/sodacar/api/Application.java
@@ -0,0 +1,16 @@
+package com.sodacar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bosong
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ pro-deer-api/src/main/java/com/sodacar/api/controller/AboutController.java
@@ -0,0 +1,13 @@
+package com.sodacar.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bosong
+ * @date 2019-05-21
+ */
+
+@RestController
+public class AboutController {
+
+}
