f4c136e5afbea2a8808bf1cf39e89910f8ecfb1a
medlying <medlying@gmail.com>
2019-05-23 23:32:59 +0800

    change mybatis starter to 2.0.0

---
+++ pro-deer-api/src/main/java/com/sodacar/deer/api/controller/DeviceController.java
@@ -0,0 +1,9 @@
+package com.sodacar.deer.api.controller;
+
+/**
+ * @author bosong
+ * @date 2019-05-23
+ */
+
+public class DeviceControllerr {
+}
---
+++ pro-deer-service/src/main/java/com/sodacar/deer/service/DeviceService.java
@@ -0,0 +1,9 @@
+package com.sodacar.deer.service;
+
+/**
+ * @author bosong
+ * @date 2019-05-23
+ */
+
+public interface DeviceService {
+}
