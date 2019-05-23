a673089288445dc9bd62a85bfe34c42729e6d6b7
medlying <medlying@gmail.com>
2019-05-23 13:32:27 +0800

    chang

--- pro-deer-risk/src/main/java/com/sodacar/risk/Application.java
+++ pro-deer-risk/src/main/java/com/sodacar/risk/Application.java
@@ -1,9 +1,18 @@
 package com.sodacar.risk;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author bosong
  * @date 2019-05-23
  */
 
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
--- pro-deer-task/src/main/java/com/sodacar/task/Application.java
+++ pro-deer-task/src/main/java/com/sodacar/task/Application.java
@@ -1,9 +1,17 @@
 package com.sodacar.task;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author bosong
  * @date 2019-05-23
  */
 
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
